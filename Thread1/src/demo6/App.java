3cf00493cc4f34c1918d8acbfdc35af53afbd8dc
Chaitanya Tanwar <chaitanya.tanwar@accolite.com>
2017-01-07 15:19:42 +0530

    Commit 2

---
+++ Thread1/src/demo6/App.java
@@ -0,0 +1,11 @@
+package demo6;
+
+public class App {
+
+	public static void main(String[] args) {
+		Worker objWorker = new Worker();
+		objWorker.main();
+		
+	}
+
+}
---
+++ Thread1/src/demo6/Worker.java
@@ -0,0 +1,91 @@
+package demo6;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Worker {
+
+	private Random random = new Random(); 
+	private Object lock1 = new Object();
+	private Object lock2 = new Object();
+	private List<Integer> list1 = new ArrayList<Integer>();
+	private List<Integer> list2 = new ArrayList<Integer>();
+	
+	public void stageOne(){
+		synchronized (lock1) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			list1.add(random.nextInt(100));
+		}		
+	}
+	
+	public void stageTwo(){
+		synchronized (lock2) {
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			list2.add(random.nextInt(100));
+		}
+	}
+	
+	public void process(){
+		for(int i = 0; i <1000;i++){
+			stageOne();
+			stageTwo();
+		}
+	}
+	
+	public void main() {
+		System.out.println("Starting : ");
+		long start = System.currentTimeMillis();
+		//process();
+		
+		/*new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				process();				
+			}
+		}).start();*/
+		
+
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				process();				
+			}
+		});
+		Thread t2 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				process();				
+			}
+		});
+		t1.start();
+		t2.start();
+		
+		try {
+			t1.join();
+			t2.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long end = System.currentTimeMillis();
+		
+		System.out.println("Time Taken : "+ (end - start));
+		System.out.println("List 1 : " + list1.size());
+		System.out.println("List 2 : " + list2.size());
+		
+	}
+}
