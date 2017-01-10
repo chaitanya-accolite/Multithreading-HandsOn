d07184520654bfb747ee63d2a43f1c202944cfe6
Aravind Balla <Aravind Balla>
2017-01-10 21:48:39 +0530

    empty folder added

---
+++ Thread1/src/assignment/PCApp.java
@@ -0,0 +1,10 @@
+package com.training.assignment;
+
+public class PCApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Thread1/src/assignment/Worker.java
@@ -0,0 +1,6 @@
+package com.training.assignment;
+
+public class Worker {
+	
+
+}
